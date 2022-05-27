848469a595c0572ad68f88f218587c3eaeb6a58d
BillC424 <wcurran44@gmail.com>
2022-05-27 17:59:00 -0400

    Added event listeners to all number buttons that display the number selected in the display div

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Functions for operations
+
 const add = function(...args) {
 	return args.reduce((previous, current) => {
     return previous + current;
@@ -36,4 +38,29 @@ const operate = function(number, operator, number) {
         return divide(number,number)
     } 
 }
-  
+
+// Variables for storing calc input 
+
+let firstNum = "input from number buttons before operator is selected";
+let operator = "operator button that is selected"
+let secondNum = "input from number buttons until = or another operator is selected"
+
+// DOM references 
+
+let display = document.getElementById('display');
+
+
+// Functions for event listeners
+
+function populateDisplay (number) {
+    display.textContent = number;
+ };
+
+// Button event listeners
+
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach( (numberButton) => { numberButton.addEventListener ('click', e => populateDisplay(e.target.id) ); } );
+
+
+
+ 
