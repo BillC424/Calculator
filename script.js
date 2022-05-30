53b8dab2a1cbdc2620660255bd9ecfbda6c11120
BillC424 <wcurran44@gmail.com>
2022-05-29 21:49:11 -0400

    Modified operatorSelected function to remove event listeners tracking and display the first number selected to instead track and display the second number selected

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let display = document.getElementById('display');
 // Variables for storing calc input 
 
 let firstNum = "";
-let operator = "operator button that is selected"
+let operator = "";
 let secondNum = "";
 
 // Functions for operations
@@ -52,23 +52,32 @@ const operate = function(number, operator, number) {
 // Functions for event listeners
 
 function populateDisplay (number) {
-    let stringToNumber = Number(number);
+    
     firstNum = firstNum + number;
-    display.textContent = number;
-    console.log(firstNum);
-   
+    display.textContent = firstNum;
+    console.log(firstNum)
+    
  };
 
- function operatorSelected (operator) {
+ function operatorSelected (operatorClicked) {
     
+    operator = operatorClicked;
     console.log(operator);
-   
+    numberButtons.forEach( (numberButton) => { numberButton.removeEventListener ('click', e => populateDisplay(e.target.id) ); } );
+    numberButtons.forEach( (numberButton) => { numberButton.addEventListener ('click', e => secondNumDisplay(e.target.id) ); } );
  };
+
+ function secondNumDisplay (number) {
+    secondNum = secondNum + number;
+    display.textContent = secondNum;
+    console.log(secondNum)
+ }
 
 // Button event listeners
 
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach( (numberButton) => { numberButton.addEventListener ('click', e => populateDisplay(e.target.id) ); } );
+
 
 const operatorButtons = document.querySelectorAll('.operation');
 operatorButtons.forEach( (operatorButton) => { operatorButton.addEventListener ('click', e => operatorSelected(e.target.id)  ); } );
